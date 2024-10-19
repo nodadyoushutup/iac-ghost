d688bd162072c62d4280a39465ebc8642b8896d6
NoDadYouShutUp <70164187+nodadyoushutup@users.noreply.github.com>
2024-10-19 18:31:50 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,11 +6,3 @@ terraform {
     }
   }
 }
-
-resource "spacelift_environment_variable" "tf_log" {
-  context_id  = "config"
-  name        = "TF_LOG"
-  value       = "debug"
-  write_only  = false
-  description = "Terraform log level"
-}
