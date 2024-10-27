77caf2e94f850104a7423155d7682dda174e1579
nodadyoushutup <admin@nodadyoushutup.com>
2024-10-27 19:40:47 +0000

    restructure

--- terraform/spacelift/main.tf
+++ terraform/spacelift/main.tf
@@ -1,13 +1,3 @@
-# provider
-terraform {
-  required_providers {
-    spacelift = {
-      source  = "spacelift-io/spacelift"
-      version = ">= 0.2.0"
-    }
-  }
-}
-
 resource "random_id" "trigger" {
     byte_length = 8
 }
